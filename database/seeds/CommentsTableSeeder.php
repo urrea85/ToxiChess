5082d305f6581cdb09eb7d4d169b0eacd2fe5136
Daniel Perez <dpm63@gcloud.ua.es>
2021-03-11 16:55:22 +0100

    Add simple seeder for comment

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Comment;
+use Illuminate\Database\Seeder;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comment = new Comment(['content'=>'Move e7! its check!']);
+        $comment->save();
+    }
+}
