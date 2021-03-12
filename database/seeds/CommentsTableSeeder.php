c433f653285bcaad675d94b1626b177b3492b9b9
Daniel Perez <dpm63@gcloud.ua.es>
2021-03-12 22:26:52 +0100

    create seeder for comments table

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Comment;
+use App\Game;
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CommentsTableSeeder extends Seeder
 {
@@ -12,7 +15,30 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('comments')->delete();
         $comment = new Comment(['content'=>'Move e7! its check!','color'=>false]);
+        $comment->game()->associate(Game::all()->random());
+        $comment->user()->associate(User::all()->random());
+        $comment->save();
+
+        $comment = new Comment(['content'=>'Nooooooooo','color'=>true]);
+        $comment->game()->associate(Game::all()->random());
+        $comment->user()->associate(User::all()->random());
+        $comment->save();
+
+        $comment = new Comment(['content'=>'We are going to lose!1!!!1!','color'=>false]);
+        $comment->game()->associate(Game::all()->random());
+        $comment->user()->associate(User::all()->random());
+        $comment->save();
+
+        $comment = new Comment(['content'=>'Take care, the best move is Kf3! ','color'=>true]);
+        $comment->game()->associate(Game::all()->random());
+        $comment->user()->associate(User::all()->random());
+        $comment->save();
+
+        $comment = new Comment(['content'=>'Yeeeeeeeeeeeeees','color'=>false]);
+        $comment->game()->associate(Game::all()->random());
+        $comment->user()->associate(User::all()->random());
         $comment->save();
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        #$this->call(CommentsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(GamesTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
     }
 }
