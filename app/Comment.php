a0cbbaf37766a99c7ab07693c700ee9a64384493
Daniel Perez <dpm63@gcloud.ua.es>
2021-03-12 20:57:50 +0100

    make models Comment and User

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function game(){
+        return $this->belongsTo('App\Game');
+    }
 }
