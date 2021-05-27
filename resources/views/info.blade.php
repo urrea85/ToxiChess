9ba60900cf99ae092768ee208d898cd77f6b4856
EricSanchezTamayo <est20@alu.ua.es>
2021-05-27 16:20:58 +0200

    info done

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -11,7 +11,8 @@
 <style> 
 .check-list {
   margin: 0;
-  margin-top:30%;
+  margin-top:5%;
+  margin-bottom:5%;
   padding-left: 1.2rem;
 }
 
@@ -37,7 +38,7 @@
     transform: rotate(45deg);
 }
 body{
-    text-align:center;
+    
     color: white;
 }
 
@@ -66,38 +67,70 @@ section {
   display: table;
   clear: both;
 }
+
+.left {
+    float: left;
+    width: 50%;
+}
+
+.right {
+    float: right;
+    width: 50%;
+}
+
+.embed-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    height: 0;
+    overflow: hidden;
+}
+.embed-container iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
 </style>
 
+<div class="row justify-content-center" style="margin-top: 5%">
+        <div class="col-md-8">
+            <div class="card" style=" border-color: black;">
+                <div class="card-header" style="background-color: var(--mainColor); border-color: black;"> <h3 style="margin-top:2% text-align:center;" > Welcome to Toxichess <h3></div>
+                <div class="card-body" style="background: #baadd9;">
+                  <div class="left">
+                  <FONT COLOR="black"> <h5> Your number one site for playing chess. We are dedicated to providing you with the best services, among which stands out <h5></FONT>
+              
+                  <FONT COLOR="black"><ul class="check-list">
+                            <li>Originality</li>
+                            <li>Velocity</li>
+                            <li>Interaction</li>
+                            <li>Customer Support</li>
+                            <li>Privacy</li>
+                    </ul></FONT>
 
-<h3 style="margin-top:2%"> Welcome to Toxichess <h3>
-    <h5> Your number one site for playing chess. We are dedicated to providing you with the best services, among which stands out <h5>
-<section>
-  <div>
+                  <FONT COLOR="black"><h5 style="margin-top:5%">Founded in 2021 by, Toxichess has come a long way since its inception at the University of Alicante.</h5></FONT>
 
-  <div class="row">
-  <div class="column">
-  <ul class="check-list">
-            <li>Originality</li>
-            <li>Velocity</li>
-            <li>Interaction</li>
-            <li>Customer Support</li>
-            <li>Privacy</li>
-        </ul>
-  </div>
-  <div class="column">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3127.4326395786684!2d-0.5165047846646607!3d38.38524457965286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6236bb72bf619b%3A0x506e11c403138428!2sUniversidad%20de%20Alicante!5e0!3m2!1ses!2ses!4v1621973327447!5m2!1ses!2ses" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-  </div>
-</div> 
-    
-  </div>
-</section>
+                  <FONT COLOR="black"><h5>We hope you enjoy our services as much as we enjoy offering them. If you have any questions or comments, feel free to contact us.</h5></FONT>
 
-<h5>Founded in 2021 by, Toxichess has come a long way since its inception at the University of Alicante.</h5>
+                  <FONT COLOR="black"><h5 style="margin-top:5%">Sincerely,</h5></FONT>
 
-    <h5>We hope you enjoy our services as much as we enjoy offering them. If you have any questions or comments, feel free to contact us.</h5>
-
-    <h5>Sincerely,</h5>
-
-    <h5>Toxichess</h5>
+                  <FONT COLOR="black"><h5 style="margin-top:5%">Toxichess</h5></FONT>
+                  </div>
+                  <div class="right">
+                  <FONT COLOR="black"><h5 style="text-align:center"><u>Nuestra sede</u></h5></FONT>
+                  <div class="embed-container" style="margin-top: 10%; margin-left:5%">
+                  
+                    <div class="column">
+                    
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3127.4326395786684!2d-0.5165047846646607!3d38.38524457965286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6236bb72bf619b%3A0x506e11c403138428!2sUniversidad%20de%20Alicante!5e0!3m2!1ses!2ses!4v1621973327447!5m2!1ses!2ses" style="border:0;"  loading="lazy"></iframe>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+         </div>
+      </div>
 
 @endsection
